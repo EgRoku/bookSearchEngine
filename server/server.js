5d6508cd031feea152e58a8322a804a010ed9479
EgRoku <jimenezjj5150@gmail.com>
2023-09-20 16:43:02 -0700

    changed server start over to apollo gql boilerplate. Added typeDefs

--- server/server.js
+++ server/server.js
@@ -14,16 +14,34 @@ const server = new ApolloServer({
   context: authMiddleware, 
 });
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
 
-app.use(routes);
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  server.applyMiddleware({ app });
+  app.use("/graphql", expressMiddleware(server, {}));
+
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+    });
+  }
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
