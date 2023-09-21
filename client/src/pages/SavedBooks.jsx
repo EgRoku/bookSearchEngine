4b35d9db439b13ed89ce044850add465b9c2d4e5
EgRoku <jimenezjj5150@gmail.com>
2023-09-20 23:25:16 -0700

    fixed savedbooks bug

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -1,42 +1,39 @@
-import { useQuery, useMutation } from '@apollo/client';
-import {
-  Container,
-  Card,
-  Button,
-  Row,
-  Col
-} from 'react-bootstrap';
-import { QUERY_ME } from '../utils/queries';
-import { REMOVE_BOOK } from '../utils/mutations';
-import Auth from '../utils/auth';
-import { removeBookId } from '../utils/localStorage';
+import { useQuery, useMutation } from "@apollo/client";
+import { Container, Card, Button, Row, Col } from "react-bootstrap";
+import { QUERY_ME } from "../utils/queries";
+import { REMOVE_BOOK } from "../utils/mutations";
+import Auth from "../utils/auth";
+import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
-  const [ loading, data ] = useQuery( QUERY_ME );
-  const [ removeBook ] = useMutation( REMOVE_BOOK );
+  const { loading, data } = useQuery(QUERY_ME);
+  const [removeBook] = useMutation(REMOVE_BOOK);
 
   const userData = data?.me || {};
 
-const handleDeleteBook = async(bookId) => {
-  const token = Auth.loggedIn() ? Auth.getToken() : null;
-  
-  if (!token) {
-    return false;
-  }
+  // create function that accepts the book"s mongo _id value as param and deletes the book from the database
+  const handleDeleteBook = async (bookId) => {
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-  try {
-    const { data } = await removeBook({
-      variables: { bookId },
-    });
-    console.log(data);
-    removeBookId(bookId);
-  } catch (err) {
-    console.error(err);
-  }
-};
+    if (!token) {
+      return false;
+    }
 
+    try {
+      const { data } = await removeBook({
+        variables: { bookId },
+      });
+      console.log(data);
+      // upon success, remove book"s id from localStorage
+      removeBookId(bookId);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  // if data isn't here yet, say so
   if (loading) {
-    return <h1> LOADING... </h1>;
+    return <h2>LOADING...</h2>;
   }
 
   return (
